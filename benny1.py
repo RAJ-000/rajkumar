cf49b2d6ea4924ba357a3c03c50bd289b784f6cd
rajkumar <mrajukumar000@gmail.com>
2021-12-23 17:54:51 +0530

    file

--- benny1.py
+++ benny1.py
@@ -1,3 +1,4 @@
 firstname = input("enter your first name : ")
+middlename = input("enter your middle name : ")
 lastname = input("enter your last name : ")
-print("your full name is ",firstname,lastname)
+print("your full name is ",firstname,middlename,lastname)
